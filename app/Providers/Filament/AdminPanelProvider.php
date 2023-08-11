b639d8ff1a37e716e376e39abeb795f1044c74af
imanfirdaus87 <imanfirdaus87@gmail.com>
2023-08-11 17:26:51 +0700

    nambah registrasi

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
             ->topNavigation()
             ->colors([
                 'primary' => Color::Amber,
